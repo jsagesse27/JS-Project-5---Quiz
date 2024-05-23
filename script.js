bb95ff72d7934c1666e279758050c3e47bf4168d
Jovens Sagesse <jmastaex@gmail.com>
2024-05-23 16:48:00 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,175 @@
+(function(){
+    // Functions
+    function buildQuiz(){
+      // variable to store the HTML output
+      const output = [];
+  
+      // for each question...
+      myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+  
+          // variable to store the list of possible answers
+          const answers = [];
+  
+          // and for each available answer...
+          for(letter in currentQuestion.answers){
+  
+            // ...add an HTML radio button
+            answers.push(
+              `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
+            );
+          }
+  
+          // add this question and its answers to the output
+          output.push(
+            `<div class="slide">
+              <div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join("")} </div>
+            </div>`
+          );
+        }
+      );
+  
+      // finally combine our output list into one string of HTML and put it on the page
+      quizContainer.innerHTML = output.join('');
+    }
+  
+    function showResults(){
+  
+      // gather answer containers from our quiz
+      const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+      // keep track of user's answers
+      let numCorrect = 0;
+  
+      // for each question...
+      myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+        // if answer is correct
+        if(userAnswer === currentQuestion.correctAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+  
+          // color the answers green
+          answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else{
+          // color the answers red
+          answerContainers[questionNumber].style.color = 'red';
+        }
+      });
+  
+      // show number of correct answers out of total
+      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+  
+    function showSlide(n) {
+      slides[currentSlide].classList.remove('active-slide');
+      slides[n].classList.add('active-slide');
+      currentSlide = n;
+      if(currentSlide === 0){
+        previousButton.style.display = 'none';
+      }
+      else{
+        previousButton.style.display = 'inline-block';
+      }
+      if(currentSlide === slides.length-1){
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+      }
+      else{
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+      }
+    }
+  
+    function showNextSlide() {
+      showSlide(currentSlide + 1);
+    }
+  
+    function showPreviousSlide() {
+      showSlide(currentSlide - 1);
+    }
+  
+    // Variables
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+      {
+        question: "Which team won the NBA championship in 2016?",
+        answers: {
+          a: "Golden State Warriors",
+          b: "Cleveland Cavaliers",
+          c: "San Antonio Spurs"
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "Who was the NBA MVP for the 2019-2020 season?",
+        answers: {
+          a: "LeBron James",
+          b: "Giannis Antetokounmpo",
+          c: "James Harden"
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "Which player scored 70 points in a single game in 2017?",
+        answers: {
+          a: "Stephen Curry",
+          b: "Klay Thompson",
+          c: "Devin Booker",
+          d: "Damian Lillard"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "Which team had the best regular-season record in the 2015-2016 NBA season?",
+        answers: {
+          a: "Golden State Warriors",
+          b: "Cleveland Cavaliers",
+          c: "San Antonio Spurs",
+          d: "Toronto Raptors"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "Who was the first overall pick in the 2018 NBA Draft?",
+        answers: {
+          a: "Luka Dončić",
+          b: "Deandre Ayton",
+          c: "Trae Young",
+          d: "Marvin Bagley III"
+        },
+        correctAnswer: "b"
+      }
+    ];
+  
+    // Kick things off
+    buildQuiz();
+  
+    // Pagination
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+  
+    // Show the first slide
+    showSlide(currentSlide);
+  
+    // Event listeners
+    submitButton.addEventListener('click', showResults);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
+  })();
